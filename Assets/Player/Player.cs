e7b36ad2098e25f77ace3de3c2e2d3ceece72109
hahns_pena <hpena02@calpoly.edu>
2026-01-11 15:14:42 -0800

    walkin

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -3,29 +3,50 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Basics")]
     private Rigidbody2D rb;
-    public float speed;
+    private SpriteRenderer sr;
+    private Animator anim;
     public InputActionReference moveAction;
+
+    [Header("Basics")]
+    public float speed;
     private Vector2 _moveDirection;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         _moveDirection = moveAction.action.ReadValue<Vector2>();
-        
-        Debug.Log(_moveDirection);
+
+        if (rb)
+        {
+            if (_moveDirection.x < 0)
+            {
+                sr.flipX = true;
+            }
+            else if (_moveDirection.x > 0)
+            {
+                sr.flipX = false;
+            }
+            anim.SetFloat("speed", _moveDirection.magnitude);
+        }
     }
 
     void FixedUpdate()
     {
+        if (rb)
+        {
+            rb.linearVelocity = _moveDirection * speed;
+        }
         
-        rb.linearVelocity = new Vector2(_moveDirection.x * speed, _moveDirection.y * speed);
         
     }
 }
